976cc03d0e964b801b7dd82d9a14f600bbf9db88
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-12-04 23:36:46 -0300

    feat:  add refactor, smart ptr and encapsulation

--- includes/core/core.hpp
+++ includes/core/core.hpp
@@ -3,8 +3,10 @@
 
 #include <GL/glew.h>
 #include <SDL.h>
+#include <memory.h>
 
 #include <iostream>
+#include <memory>
 
 namespace core {
 
@@ -14,18 +16,50 @@ struct core_renderer {
 };
 
 struct core_rgb {
+ private:
   Uint8 red{};
   Uint8 green{};
   Uint8 blue{};
+  Uint8 alpha{};
+
+ public:
+  core_rgb() = default;
+  core_rgb(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+      : red(r), green(g), blue(b), alpha(a) {}
+
+  void set_rgb(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+  Uint8 get_red() const;
+  Uint8 get_green() const;
+  Uint8 get_blue() const;
+  Uint8 get_alpha() const;
 };
 
 struct core_running {
+ private:
   bool running{true};
+
+ public:
+  core_running() = default;
+  core_running(bool running_state) : running(running_state) {}
+
+  void set_running(bool new_state);
+  bool get_running() const;
 };
 
 struct core_screen {
+ private:
   int width{};
   int height{};
+
+ public:
+  core_screen() = default;
+  core_screen(int w, int h) : width(w), height(h) {}
+
+  void set_resolution(int w, int h);
+  void set_width(int w);
+  void set_height(int h);
+  int get_width() const;
+  int get_height() const;
 };
 
 class core_interfaces {
@@ -33,9 +67,9 @@ class core_interfaces {
   virtual ~core_interfaces() = default;
   virtual bool init_sdl2() = 0;
   virtual bool create_sdl2_window(core_renderer& renderer,
-                                  core_screen& screen) = 0;
+                                  std::shared_ptr<core_screen>screen) = 0;
   virtual void background_color_sdl2(core_renderer& renderer,
-                                     core_rgb& rgb) = 0;
+                                     std::shared_ptr<core_rgb> rgb) = 0;
   virtual void quit_sdl2(core_renderer& renderer) = 0;
 };
 
@@ -44,8 +78,9 @@ class core_sdl2 : public core_interfaces {
   bool init_sdl2() override;
   void quit_sdl2(core_renderer& renderer) override;
   bool create_sdl2_window(core_renderer& renderer,
-                          core_screen& screen) override;
-  void background_color_sdl2(core_renderer& renderer, core_rgb& rgb) override;
+                          std::shared_ptr<core_screen> screen) override;
+  void background_color_sdl2(core_renderer& renderer,
+                             std::shared_ptr<core_rgb> rgb) override;
 };
 
 }  // namespace core
