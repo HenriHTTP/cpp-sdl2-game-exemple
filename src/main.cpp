0ea8f67b5c4ccf9281193dfd933d8babfb4fc673
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2023-12-07 23:51:32 -0300

    feat: use pragma header index and remove comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
-#include <memory.h>
-
-#include <memory>
-
-#include "../includes/core/core.hpp"
-#include "../includes/event/event.hpp"
-#include "../includes/object/object.hpp"
+#include "../includes/game.hpp"
 
 int main() {
   // renderer instance
@@ -29,33 +23,24 @@ int main() {
   // rgb instance
   std::shared_ptr<core::core_rgb> rgb =
       std::make_shared<core::core_rgb>(255, 255, 255, 255);
-  // rgb->red = 255;
-  // rgb->blue = 255;
-  // rgb->green = 255;
 
   app->background_color_sdl2(render, rgb);
 
   // event instance
   std::shared_ptr<event::event_sdl2> event_processor =
       std::make_shared<event::event_sdl2>();
+
   event::listener_event events;
 
   // frame instance
-  std::shared_ptr<object::object_moviment> move =
+  std::shared_ptr<object::object_moviment> object =
       std::make_shared<object::object_moviment>();
 
   std::shared_ptr<object::object_rectangle_attributes> frame =
       std::make_shared<object::object_rectangle_attributes>();
 
-  // frame.rectangle.w = 75;
-  // frame.rectangle.h = 75;
-  // frame.rectangle.x = screen->get_width() / 2 - frame.rectangle.w / 2;
-  // frame.rectangle.y = screen->get_height() / 2 - frame.rectangle.h / 2;
-  // frame.speed_x = 5;
-  // frame.speed_y = 5;
-
-  frame->set_width(75); 
-  frame->set_height(75); 
+  frame->set_width(75);
+  frame->set_height(75);
   frame->set_position_center(screen);
   frame->set_speed_x(5);
   frame->set_speed_y(5);
@@ -71,7 +56,6 @@ int main() {
   while (loop->get_running()) {
     Uint32 frameStart = SDL_GetTicks();
     glClear(GL_COLOR_BUFFER_BIT);
-    // SDL_GL_SwapWindow(render.window);
 
     if (!loop->get_running()) {
       std::cerr << "Failed: loop is not running" << '\n';
@@ -79,49 +63,19 @@ int main() {
       return -1;
     }
 
-    // while (SDL_PollEvent(&event) != 0) {
-    //   if (event.type == SDL_QUIT) {
-    //     std::cerr << "Menssage: loop is end" << std::endl;
-    //     app->quit_sdl2(render);
-    //     return -1;
-    //   }
-    //   if (event.type == SDL_KEYDOWN) {
-    //     if (event.key.keysym.sym == SDLK_ESCAPE) {
-    //       std::cerr << "Menssage: SDL quit and close " << std::endl;
-    //       app->quit_sdl2(render);
-    //       return -1; ;
-    //     }
-    //   }
-    // }
-
     while (SDL_PollEvent(&events.event) != 0) {
       if (event_processor->simple_events(events)) {
         loop->set_running(false);
       }
       if (events.event.button.button == SDL_BUTTON_LEFT) {
-        if (move->object_clicked(events, frame)) {
+        if (object->object_clicked(events, frame)) {
           std::cerr << "acertou o objeto" << '\n';
-          move->random_position_x(frame, screen);
+          object->random_position_x(frame, screen);
         }
       }
     }
 
-    // // Move the rectangle automatically
-    // frame.rectangle.x += frame.speed_x;
-    // frame.rectangle.y += frame.speed_y;
-
-    // // Invert direction when hitting the screen edges
-    // if (frame.rectangle.x < 0 ||frame.rectangle.x + frame.rectangle.w >
-    // screen.width) {
-    //   frame.speed_x = -frame.speed_x;
-    // }
-
-    // if (frame.rectangle.y < 0 || frame.rectangle.y + frame.rectangle.h >
-    // screen.height) {
-    //   frame.speed_y = -frame.speed_y;
-    // }
-    // move->auto_move(frame, screen);
-    move->bellow_move(frame, screen);
+    object->bellow_move(frame, screen);
     SDL_SetRenderDrawColor(render.renderer, 75, 0, 75, 255);
     SDL_RenderFillRect(render.renderer, &frame->get_rectangle());
     SDL_RenderPresent(render.renderer);
@@ -130,7 +84,7 @@ int main() {
     if (frameDelay > frameTime) {
       SDL_Delay(frameDelay - frameTime);
     }
-    // frame::auto_move(frame ,1280,720);
+
 
     // glMatrixMode(GL_PROJECTION);
     // glLoadIdentity();
@@ -168,7 +122,6 @@ int main() {
     // glEnd();
 
     SDL_GL_SwapWindow(render.window);
-    //  SDL_Delay(16);
     // std::cerr << "loop is running" << '\n';
     continue;
   }
